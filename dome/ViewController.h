447064be63911ea86828e9e8ea2ecd5930a96232
jiashiqi <jiashiqi@zealfi.com>
2017-01-12 08:53:12 +0800

    Initial Commit

---
+++ dome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  dome
+//
+//  Created by 贾仕琪 on 2017/1/12.
+//  Copyright © 2017年 贾仕琪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ dome/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  dome
+//
+//  Created by 贾仕琪 on 2017/1/12.
+//  Copyright © 2017年 贾仕琪. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
